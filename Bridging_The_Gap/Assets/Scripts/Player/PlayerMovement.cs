059efcd8d982ff7b910af9744793fbef0b503695
Matthew <lemtolen@ucsc.edu>
2021-04-27 14:20:19 -0700

    Added in player movement and gravity

--- Bridging_The_Gap/Assets/Scripts/Player/PlayerMovement.cs
+++ Bridging_The_Gap/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 4f;
-    [SerializeField] float maxSpeed = 10f;
+
+    [SerializeField] float groundDistance = 1f;
+    [SerializeField] float gravity = -9.81f;
+    [SerializeField] LayerMask groundMask;
+    bool isGrounded = false;
+
+    Vector3 velocity;
 
     Rigidbody rb;
+    CharacterController controller;
 
     Vector3 forward, right;
     // Start is called before the first frame update
@@ -19,11 +26,16 @@ public class PlayerMovement : MonoBehaviour
         right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
 
         rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if (Input.anyKey) Move();
+    }
+
+    void FixedUpdate() {
         if (Input.anyKey) Move();
     }
 
@@ -33,10 +45,25 @@ public class PlayerMovement : MonoBehaviour
         Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
 
         Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+        velocity = (rightMovement + upMovement);
 
         transform.forward = heading;
-        transform.position += rightMovement;
-        transform.position += upMovement;
+        // transform.position += rightMovement;
+        // transform.position += upMovement;
+
+        controller.Move(velocity);
+
+        Vector3 checkPos = new Vector3(transform.position.x, transform.position.y - 1.6f, transform.position.z);
+        if (Physics.CheckSphere(checkPos, groundDistance, groundMask))
+        {
+            isGrounded = true;
+        }
+
+        if (isGrounded && velocity.y < 0) 
+            velocity.y = -2f;
+
+        velocity.y += gravity + Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
 
         // rb.AddForce(rightMovement + upMovement);
         // rb.AddForce(upMovement);
